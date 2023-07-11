6b6b6a2e7c711f394ae013a2211c0e533a92db6e
Drychang <jimmy.chang2003@gmail.com>
2023-07-11 21:41:40 +0800

    test

--- detection.py
+++ detection.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import matplotlib.pyplot as plt
 import numpy as np
 
-%config InlineBackend.figure_format = 'retina'
+# %config InlineBackend.figure_format = 'retina'
 
 mp_drawing = mp.solutions.drawing_utils         # mediapipe 繪圖方法q
 mp_drawing_styles = mp.solutions.drawing_styles # mediapipe 繪圖樣式
